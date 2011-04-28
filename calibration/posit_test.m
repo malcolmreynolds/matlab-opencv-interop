cd047587c88e51754a6304d39f8e18feb4958579
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2011-04-28 13:44:19 +0100

    It's woooorking, it's woooorking

--- calibration/posit_test.m
+++ calibration/posit_test.m
@@ -7,25 +7,33 @@ model_points = [0 0 0; ...
                 s 0 0; ...
                 0 s 0; ...
                 0 0 s]';
-num_points = 4;
+num_points = size(model_points, 2);
 
-k = intrinsics;
+%extract focal lengths. Since we can only give one to the posit function, use mean.. is this smart? 
+focal_x = intrinsics(1,1);
+focal_y = intrinsics(2,2);
+focal_length = (focal_x + focal_y) / 2;
 r = extrinsics.r;
 t = extrinsics.t;
 
 %convert 3d points to homogeneous and do full projective transform
-model_points_transformed = k * [r,t] * [model_points; ones(1, size(model_points,2))];
+model_points_transformed = [r,t] * [model_points; ones(1, num_points)];
 %divide to convert from 2D homogeneous to 2D cartesian
-image_points = model_points_transformed(1:2,:) ./ repmat(model_points_transformed(3,:), 2, 1)
+focal_length_mtx = [intrinsics(1,1) * ones(1, num_points); ...
+                    intrinsics(2,2) * ones(1, num_points)];
+image_points = focal_length_mtx .* model_points_transformed(1:2,:) ./ repmat(model_points_transformed(3,:), 2, 1);
 
-focal_length = (intrinsics(1,1) + intrinsics(2,2)) / 2;
 
-fprintf('calling POSIT with focal_length = %f\nintrinsics = \n', focal_length);
-fprintf('%f %f %f\n', k');
+fprintf('calling POSIT with focal_x = %f, focal_y = %f, focal_length=%f\n', ...
+        focal_x, focal_y, focal_length);
 fprintf('extrinsics r = \n');
 fprintf('%f %f %f\n', r');
 fprintf('extrinsics t = %f %f %f\n', t);
 
-[r t] = posit(model_points, image_points, focal_length, int32(500))
+[r t] = posit(model_points, image_points, focal_length, int32(500));
+
+fprintf('computed r =\n');
+fprintf('%f %f %f\n', r');
+fprintf('computed t = %f %f %f\n', t);
 
                                                   
