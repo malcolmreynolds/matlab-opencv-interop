3e045190c548a161537b782dd612b7780b5dfd48
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2011-11-10 17:34:44 +0000

    Good params sorted.

--- random_forest/rf_good_params.m
+++ random_forest/rf_good_params.m
@@ -1,4 +1,6 @@
-function opts = rf_good_params
-opts.max_tree_count = int32(200);
-opts.regression_accuracy = 1e-13;
-opts.forest_accuracy = 1e-13;
+function opts = rf_good_params(num_variables)
+	% Returns a set of parameters which hopefully work well.
+	opts.max_tree_count = int32(200);
+	opts.regression_accuracy = 1e-13;
+	opts.forest_accuracy = 1e-13;
+end
