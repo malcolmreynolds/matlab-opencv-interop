e4a7c24f1eeb4e46f2b0be26ce76ac4a145050ca
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2011-11-10 17:35:06 +0000

    Smart training done.

---
+++ random_forest/smart_training/rf_train_with_param_search.m
@@ -0,0 +1,70 @@
+function [best_params stats] = rf_train_with_param_search(training_x, training_y, search_range)
+	% Given some x and y to train on, perform a parameter search to determine what some good parameters
+	% are for this data. Uses out-of-bag (oob) error currently, could be modified to do cross validation?
+	
+	% need to extract this as the default heuristic for setting the num_active_vars is square root of number
+	% of variables
+	num_vars = size(training_x,2);
+	sqrt_num_vars = sqrt(num_vars);
+	default_options = {{'max_trees_rng', [5 10 25 50 100 200]}, ...
+	                   {'max_depth_rng', [5 10 15 20 25 30]}, ...
+	                   {'regression_accuracy_rng', [10 0.1 0.001]}, ...
+	                   {'num_active_vars_rng', ceil(sqrt_num_vars*[sqrt_num_vars 3 2 1 0.5])}};
+	
+	% If no options supplied, give some sensible defaults					
+	if nargin < 3,
+		search_ranges = struct();
+	end
+	for i=1:length(default_options),
+		name = default_options{i}{1};
+		if ~isfield(search_ranges, name),
+			setfield(search_ranges, name, default_options{i}{2});
+		end
+	end
+	
+	% Trim the num_vars list in case it specified anything higher than the number of variables
+	valid_num_vars_mask = search_ranges.num_active_vars_rng <= num_vars;
+	search_ranges.num_active_vars_rng = search_ranges.num_active_vars_rng(valid_num_vars_mask);
+	
+	% Prepare the output array
+	stats.oob_error = zeros(length(search_range.max_trees_rng), ...
+	                        length(search_range.max_depth_rng), ...
+	                        length(search_range.reg_accuracy_rng), ...
+	                        length(search_range.num_active_vars.rng));
+	stats.time = zeros(length(search_range.max_trees_rng), ...
+					   length(search_range.max_depth_rng), ...
+					   length(search_range.reg_accuracy_rng), ...
+					   length(search_range.num_active_vars.rng));
+	stats.num_trees = zeros(length(search_range.max_trees_rng), ...
+							length(search_range.max_depth_rng), ...
+							length(search_range.reg_accuracy_rng), ...
+							length(search_range.num_active_vars.rng));
+	
+	fprintf('running exhaustive search...')
+	
+	opts = rf_good_params()
+	for w=1:length(search_range.max_trees_rng),
+		opts.max_tree_count = int32(search_range.max_trees_rng(w));
+		for x=1:length(search_range.max_depth_rng), 
+			opts.max_depth = int32(search_range.max_depth_rng(x));
+			for y=1:length(search_range.regression_accuracy_rng),
+				opts.regression_accuracy = search_range.regression_accuracy_rng(y);
+				for z=1:length(search_range.num_active_vars_rng),
+					opts.num_active_vars = int32(search_range.num_active_vars_rng(z));
+					
+					% Train the forest, record the stats, and delete
+					[forest stats.time(w,x,y,z)] = rf_train(training_x, training_y, opts);
+					forest_info = rf_info(forest);
+					stats.oob(w,x,y,z) = forest_info.oob_error;
+					stats.num_trees(w,x,y,z) = forest_info.num_trees;
+					rf_delete(forest);
+				end
+			end
+			fprintf('dont max depth = %d\n', opts.max_depth);
+		end
+		fprintf('done max trees = %d\n', opts.max_tree_count);
+	end
+	
+					
+					
+		
